88da56cad2660c5a29dece37df6d8930add6db33
akoot <akoot@akoot.me>
2024-06-18 16:14:27 -0400

    WE DID IT!!!

--- src/main/kotlin/co/akoot/plugins/alleycat/AlleyCat.kt
+++ src/main/kotlin/co/akoot/plugins/alleycat/AlleyCat.kt
@@ -1,11 +1,13 @@
 package co.akoot.plugins.alleycat
 
+import co.akoot.plugins.bluefox.BlueFox
 import org.bukkit.plugin.java.JavaPlugin
 
 class AlleyCat : JavaPlugin() {
 
     override fun onEnable() {
         logger.info("I'll see what I can do...")
+        BlueFox.trace("hehehe.. - AlleyCat") // Test that PROVES we can reference BlueFox!!
     }
 
     override fun onDisable() {
