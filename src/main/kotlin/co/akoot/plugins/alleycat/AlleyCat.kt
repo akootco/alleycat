21cdc3d4bdf54ac32fa4a5d31b9e3121eb17a60f
akoot <akoot@akoot.me>
2024-07-30 14:39:56 -0400

    Created KTP plugin which adds the kotlin library to the classpath. All plugins now exclude the kotlin library and load it in runtime

--- src/main/kotlin/co/akoot/plugins/alleycat/AlleyCat.kt
+++ src/main/kotlin/co/akoot/plugins/alleycat/AlleyCat.kt
@@ -14,34 +14,58 @@ import org.bukkit.OfflinePlayer
 import org.bukkit.plugin.java.JavaPlugin
 import java.util.*
 
+/**
+ * A FoxPlugin that aims to take care of administration aspects of the server
+ */
 class AlleyCat : FoxPlugin() {
 
     override fun register() {
-        logger.info("I'll see what I can do...")
+        logger.info("I'll see what I can do...!")
     }
 
     override fun unregister() {
         // Plugin shutdown logic
     }
 
+    /**
+     * Bans a player
+     *
+     * @param player The player
+     * @param source The person that banned the player
+     * @param reason The reason the player is banned
+     * @param expiration The date the ban expires. If null, it will never expire (unless pardoned) (Default = null)
+     * @return true if the player is banned, false otherwise
+     */
     fun ban(player: OfflinePlayer, source: String, reason: String = "Banned by an operator.", expiration: Date? = null): Boolean {
         if (player.isBanned) return false
-        // player.ban<BanEntry<PlayerProfile>>("hey", Date(), "me")
-        player.banPlayer(reason, expiration, source)
+        player.ban<BanEntry<PlayerProfile>>(reason, expiration, source)
         return true
     }
 
-    fun pardon(player: OfflinePlayer): Boolean {
+    /**
+     * Pardons a player
+     *
+     * @param player The player
+     * @param reason The reason the player was pardoned. If null, a message won't be sent. (Default = null)
+     * @return true if the player was pardoned (banned in the first place), false otherwise
+     */
+    fun pardon(player: OfflinePlayer, reason: String? = null): Boolean {
         val banList = server.getBanList(BanListType.PROFILE)
         val entry = banList.getBanEntry(player.playerProfile) ?: return false
         entry.remove()
         return true
     }
 
+    /**
+     * Register all the commands for this plugin
+     */
     override fun registerCommands() {
         registerCommand(TestCommand(this))
     }
 
+    /**
+     *
+     */
     fun getBanQuery(entry: BanEntry<PlayerProfile>, showCreated: Boolean = false, anonymous: Boolean = false, now: Long = System.currentTimeMillis()): Component {
         val hasMessage = entry.reason != "Banned by an operator."
         val hasExpiration = entry.expiration != null
@@ -55,6 +79,9 @@ class AlleyCat : FoxPlugin() {
             .append(if(hasExpiration) entry.expiration?.time?.let { TimeUtil.getTimeComponent(it, now) } ?: Component.text("NEVER").color(ColorUtil.getColor("number")) else TextUtil.EMPTY)
     }
 
+    /**
+     *
+     */
     fun getBanQuery(player: OfflinePlayer, showCreated: Boolean = false, anonymous: Boolean = false, now: Long = System.currentTimeMillis()): Component {
         val banList = server.getBanList(BanListType.PROFILE)
         val entry = banList.getBanEntry(player.playerProfile) ?: return Component.text("${player.name} is not banned.").color(ColorUtil.getColor("error_text"))
--- src/main/kotlin/co/akoot/plugins/alleycat/commands/TestCommand.kt
+++ src/main/kotlin/co/akoot/plugins/alleycat/commands/TestCommand.kt
@@ -2,18 +2,25 @@ package co.akoot.plugins.alleycat.commands
 
 import co.akoot.plugins.bluefox.api.FoxCommand
 import co.akoot.plugins.bluefox.api.FoxPlugin
-import co.akoot.plugins.bluefox.util.ColorUtil
-import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.format.TextColor
+import co.akoot.plugins.bluefox.util.Txt
 import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 
 class TestCommand(plugin: FoxPlugin): FoxCommand(plugin, "test", "t") {
+
     override fun onTabComplete(sender: CommandSender, args: Array<out String>?): MutableList<String> {
         return mutableListOf()
     }
 
     override fun onCommand(sender: CommandSender, args: Array<out String>?): Boolean {
-        sender.sendMessage(Component.text("win?").color(ColorUtil.getColor("error_text")))
+        if (sender !is Player) return false
+        sendMessage(sender, "Hello @PLAYER, today is the #NUMBERst day of the year!", "PLAYER" to sender.name, "NUMBER" to 101)
+        sendMessage(sender, "@PLAYER, today is the #NUMBERst day of the year!", "PLAYER" to sender.displayName(), "NUMBER" to 101)
+        sender.sendMessage(Txt("hey").c)
+        sender.sendMessage(Txt("hey", 0xff0000).c)
+        sender.sendMessage(Txt("do not click here pal", 0xff0000).run("/kill").c)
+        sender.sendMessage(Txt("do not click here pal", 0xff0000).hover("buddy..?").run("/kill").c)
+        sender.sendMessage(Txt("do not click here pal", 0xff0000).hover(Txt("buddy..?", 0x00aa00)).run("/kill").c)
         return true
     }
 }
