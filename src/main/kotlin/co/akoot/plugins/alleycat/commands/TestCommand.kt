48fc436d5935266d567108bce6c5dd2d080b5476
Akoot <akoot@akoot.me>
2025-02-04 20:51:45 -0500

    Added new Text features - Renamed Txt.kt to Text.kt - Text is now separated from the "root" node so hover/click events are their own thing - Text.kt now includes a few String, Component, and TextComponent.Builder extensions - New constructors for Text.kt - Combine Text, Component, TextComponent, TextComponent.Builder, and String objects - send(audience) and broadcast(permission) options - erm! just look at Text.kt!

--- src/main/kotlin/co/akoot/plugins/alleycat/commands/TestCommand.kt
+++ src/main/kotlin/co/akoot/plugins/alleycat/commands/TestCommand.kt
@@ -3,9 +3,13 @@ package co.akoot.plugins.alleycat.commands
 import co.akoot.plugins.bluefox.BlueFox
 import co.akoot.plugins.bluefox.api.FoxCommand
 import co.akoot.plugins.bluefox.api.FoxPlugin
-import co.akoot.plugins.bluefox.util.Txt
+import co.akoot.plugins.bluefox.util.Text
+import co.akoot.plugins.bluefox.util.Text.Companion.titleCase
+import co.akoot.plugins.bluefox.util.Text.EnumOption
+import net.kyori.adventure.audience.Audience
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import kotlin.random.Random
 
 class TestCommand(plugin: FoxPlugin) : FoxCommand(plugin, "test", "t") {
 
@@ -25,7 +29,7 @@ class TestCommand(plugin: FoxPlugin) : FoxCommand(plugin, "test", "t") {
         )
         sendMessage(
             sender,
-            "Hello @PLAYER, today is the #{NUMBER}st day of the year!",
+            "Hello @PLAYER, today is the #{NUMBER}st day of the year (this is a)!",
             "PLAYER" to sender.displayName(),
             "NUMBER" to 101
         )
@@ -37,6 +41,13 @@ class TestCommand(plugin: FoxPlugin) : FoxCommand(plugin, "test", "t") {
         sendMessage(sender, "@PLAYER hello", "PLAYER" to sender.name)
         sendMessage(sender, "It is now \$TIME", "TIME" to BlueFox.settings.getLong("time"))
         BlueFox.settings.set("time", System.currentTimeMillis())
+        (Text("Click ") + Text("HERE", "accent").underlined().hover("yes").suggest("win") + Text(" to WIN!")).send(sender)
+        (Text("Click ") + Text("HERE", "error_accent").underlined().strikethrough().hover("no").suggest("die") + Text(" to WIN!")).broadcast()
+        (Text("win!") * 3).broadcast()
+        (Text() + "There are " + Random.nextInt() + " more seconds to go.").broadcast()
+        Text.newline(sender)
+        val item = sender.inventory.itemInMainHand
+        (Text(EnumOption.TITLE_CASE, EnumOption.SPACES) + sender + " is holding " + item.amount + " " + item + " which is of type: " + item.type + "!").boldItalic().send(sender)
         return true
     }
 }
